e24cbb285477e5c2cd3ce5a0f8a23dc6a400f6da
Seokho Youn <axlover@hotmail.com>
2018-12-11 11:54:07 +0900

    랜덤값 맞추기 게임 구현

--- src/com/week4/Number0k.java
+++ src/com/week4/Number0k.java
@@ -5,12 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Number0k {
+	static int count = 0;
 
 	public static void main(String[] args) {
 		Number0k game = new Number0k();
 		int ans = (int)(Math.random()*100)+1; // 정답
 		try {
-			while(game.questionNumber(game.getNumber(), ans) != 0);
+			while(true) {
+				System.out.print("숫자 입력 : ");
+				int key = game.getNumber();
+				int exit = game.questionNumber(key, ans);
+				if(exit == 0) 
+					break;
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -19,18 +26,19 @@ public class Number0k {
 	// 키보드 입력용 메서드
 	public int getNumber() throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		return in.read();
+		return Integer.parseInt(in.readLine());
 	}
 	
 	public int questionNumber(int i, int ans) {
-		if(ans > i) {
-			System.out.println(i+"보다 큽니다"); return 1;
+		count++;
+		if(i < ans) {
+			System.out.println(i+" 보다 큽니다("+count+")"); return 1;
 		}
-		else if(ans < i) {
-			System.out.println(i+"보다 작습니다"); return -1;
+		else if(i > ans) {
+			System.out.println(i+" 보다 작습니다("+count+")"); return -1;
 		}
 		else {
-			System.out.println("정답입니다."); return 0;
+			System.out.println("정답입니다.("+count+")"); return 0;
 		}
 	}
 
