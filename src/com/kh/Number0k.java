4bcb0ae6708d6fe2ec1ec9f27aeebd39168696c5
Seokho Youn <axlover@hotmail.com>
2018-12-12 12:49:35 +0900

    문자 입력 및 1-100 이외의 숫자에 대한 예외 입력 처리

--- src/com/kh/Number0k.java
+++ src/com/kh/Number0k.java
@@ -1,22 +1,33 @@
 package com.kh;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Number0k {
+	static int count = 0; // 시도 횟수 저장용 카운트값 초기화
 	public static void main(String[] args) {
 		int answer = (int)(Math.random()*100)+1; // 1 ~ 100 사이의 난수 생성
+		System.out.println(answer); // *****테스트용 정답 표시
 		Number0k n = new Number0k(); // 객체 생성
-		int mine = 0, count = 0, key = 0;
+		int mine = 0, key = 0; // 내가 입력한 값, 정보 초기화 
 		while(true) {
 			count++;
-			mine = n.getNumber();
-			key = n.questionNumber(mine, answer);
-			if(key == 1)
-				System.out.println("큽니다 -"+count);
-			else if(key == -1)	
-				System.out.println("작습니다 -"+count);
-			else
-				System.out.println("정답입니다. -"+count);
+			try{
+				mine = n.getNumber();
+				if(mine < 1 || mine > 100) throw new Over100();
+			}catch(InputMismatchException e ) {			
+				System.out.println("문자는 입력할 수 없습니다."); count--; continue;
+			}catch(Over100 e2) {
+				System.out.println("1 - 100 사이의 정수만 입력 가능합니다."); count--; continue;
+			}
+				key = n.questionNumber(mine, answer);
+				if(key == 1)
+					System.out.println("정답보다 큽니다 ");
+				else if(key == -1)	
+					System.out.println("정답보다 작습니다 ");
+				else {
+					System.out.println("정답입니다.("+count+")"); break;
+				}
 		}
 	}
 	
@@ -27,6 +38,6 @@ public class Number0k {
 	}
 	
 	public int questionNumber(int mine, int ans) {
-		return (mine == ans)? 0 : (mine > ans)? 1 : -1;
+	return (mine > ans)? 1 : (mine < ans)? -1 : 0;
 	}
 }
